15cd48bd7116f7af896c23ff94c79bb30e8a8b8f
AmjadRabby <glamarrabby@gmail.com>
2020-08-08 15:49:27 +0600

    true false practic

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+//truthy
+//'0', ' ', [], {}
+
+// const name = " ";
+let name = false;
+
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
